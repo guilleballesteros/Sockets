7988b43714c3a29a8a3cf7c0d5c28a6cd80d3b15
guilleballesteros <bugfiher@outlook.com>
2020-11-23 18:14:46 +0100

    cambios

--- Cliente.py
+++ Cliente.py
@@ -1,10 +1,49 @@
 import socket
 
 s = socket.socket()
-s.connect(("localhost", 9003))
+s.connect(("localhost", 9999))
+validacion=False
 
-nom_cliente=input("Introduce tu nombre: ")
-s.send(nom_cliente.encode())
+def comprobarEmail(email):
+    comprobacion=False
+    if "@" in email:
+        nuevo_email=email.split('@')
+        resto=nuevo_email[1]
+        if "." in resto:
+            comprobacion=True
+    
+    return comprobacion
+
+
+
+while(not validacion):
+    opcion=int(input("Seleccione una accion: 1.(Iniciar Sesion) 2.(Registrar una cuenta nueva): "))
+    if ( opcion==1):
+        correo=input("Introduzca una direccion de correo: ")
+        if (comprobarEmail(correo)):
+            s.send(("I;"+correo).encode())
+            respuesta=s.recv(1024).decode()
+            print(respuesta)
+            if(respuesta=="A"):
+                validacion=True
+            else:
+                print("No se ha podido iniciar sesion, puede que el correo sea incorrdcto.")
+        else:
+            print("Correo no valido")
+    elif( opcion==2):
+        correo=input("Introduzca una direccion de correo: ")
+        if (comprobarEmail(correo)):
+            s.send(("R;"+correo).encode())
+            respuesta=s.recv(1024).decode()
+            if(respuesta=='A'):
+                print("Correo registrado exisosamente, Inicie sesion o registre un nuevo correo.")
+            else:
+                print("No se ha podido Registrar, puede que el correo ya exista.")
+        else:
+            print("Correo no valido")
+    else:
+        print("La opcion elegida no es correcta")
+opcion=s.recv(1024).decode()
 fin_partida=True
 while fin_partida:
     letra = input("Letra a buscar >> ")
